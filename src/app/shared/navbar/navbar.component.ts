33ec47bb47d5f515247ccf63a1516a29b73fc362
yassineelkasmy <yassineelkasmy@gmail.com>
2022-03-20 20:01:12 +0100

    Adjust landing and contact pages

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,18 +1,16 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @HostListener('window:scroll', ['$event'])
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onWindowScroll() {
     let element = document.querySelector('.navbar') as HTMLElement;
@@ -23,4 +21,14 @@ export class NavbarComponent implements OnInit {
     }
   }
 
+  onClickAccount() {
+    const submitMsg =
+      'We are still maintaining our new platform, coming soon 😎';
+
+    Swal.fire({
+      text: submitMsg,
+      titleText: 'Coming Soon',
+      confirmButtonText: 'Ok',
+    });
+  }
 }
--- src/app/views/landing/components/contact/contact.component.ts
+++ src/app/views/landing/components/contact/contact.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.contactForm = new FormGroup({
+      name: new FormControl('', [
+        Validators.minLength(5),
+        Validators.maxLength(30),
+        Validators.required,
+      ]),
+      company: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
+      message: new FormControl('', [Validators.required]),
+    });
   }
+  contactForm: FormGroup;
 
+  ngOnInit(): void {}
 }
